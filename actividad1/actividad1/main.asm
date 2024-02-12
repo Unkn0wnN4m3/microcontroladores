73f05f4bd862fff00db5b55403e1eb58d1dd8eef
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2024-02-11 22:16:56 -0600

    feat: port configuration applied

--- actividad1/actividad1/main.asm
+++ actividad1/actividad1/main.asm
@@ -7,6 +7,26 @@
 
 
 ; Replace with your application code
+.cseg
+.org 0x00
+
+        ; Define the ports
+        ; Making b0 = in, b1 = in and b2 = out
+        ; | * | * | * | * | * | * | * | * |
+        ; | 7 | 6 | 5 | 4 | 3 | 2 | 1 | 0 | PB
+        ;   0   0   0  0   0   1   0  0 DDRB
+        ;   0   0   0  0   0   0   0  1 PORTB
+        ;   0   0   0  0   0   0   1  0 PINB
+        ; DDRB -> in or out
+        ; PORB -> in = res on / res off; out = 1 / 0
+        ; PINB -> in = watcher
+        cbi DDRB, PB0 ; 0
+        cbi DDRB, PB1 ; 0
+
+        ; WARN: must be the builtin LED
+        sbi DDRB, PB2 ; 1
+
+        ; TCNT timer/counter
+        ; 
+
 start:
-    inc r16
-    rjmp start
