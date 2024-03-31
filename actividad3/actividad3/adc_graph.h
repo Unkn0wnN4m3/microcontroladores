469af761dc033d32fc75a42f749f2e1a7c1c1ca4
AronCruzSanJuan <159409569+AronCruzSanJuan@users.noreply.github.com>
2024-03-31 17:57:52 -0600

    se removio la libreria ADC
    se borro por que no funcionba

--- actividad3/actividad3/adc_graph.h
+++
@@ -1,9 +0,0 @@
-#ifndef ADC_GRAPH_H
-#define ADC_GRAPH_H
-
-#include "oled_i2c.h"
-
-void show_adc_graph(uint16_t adc_value, uint8_t adc_channel);
-
-#endif /* ADC_GRAPH_H */
-
--- actividad3/actividad3/main.c
+++ actividad3/actividad3/main.c
@@ -1,6 +1,6 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "adc_graph.h"
+//#include "adc_graph.h"
 
 int main(void) {
 	// Simulando valores de ADC y canal ADC
@@ -8,7 +8,8 @@ int main(void) {
 	uint8_t adc_channel = 3; // Canal del ADC
 
 	// Mostrar gráfica y texto en la pantalla OLED
-	show_adc_graph(adc_value, adc_channel);
+	// WARN:NECESITA CORRECCÓN LA LIBRERIA
+	//show_adc_graph(adc_value, adc_channel);
 
 	while (1) {
 		// Tu código principal aquí
