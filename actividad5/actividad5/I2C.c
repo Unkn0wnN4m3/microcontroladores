46747409f53b3d40d3e4392342cf3d82f71216c7
AronCruzSanJuan <159409569+AronCruzSanJuan@users.noreply.github.com>
2024-05-06 23:28:57 -0600

    SE AGREGARON LAS LIBRERIAS I2C

---
+++ actividad5/actividad5/I2C.c
@@ -0,0 +1,6 @@
+/*
+ * I2C.c
+ *
+ * Created: 06/05/2024 11:26:06 p. m.
+ *  Author: lenovo
+ */ 
---
+++ actividad5/actividad5/I2C.h
@@ -0,0 +1,16 @@
+/*
+ * I2C.h
+ *
+ * Created: 06/05/2024 11:26:30 p. m.
+ *  Author: lenovo
+ */ 
+
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+
+
+
+#endif /* I2C_H_ */
